3b339f30abac7e638b4518924ed1aba409c84a76
ryokatsu <ryokatsu0719@gmail.com>
2023-12-30 14:28:32 +0900

    fix:if else and or

--- lox-java/lox/Parser.java
+++ lox-java/lox/Parser.java
@@ -24,7 +24,7 @@ class Parser {
     return assignment();
   }
   private Expr assignment () {
-    Expr expr = equality();
+    Expr expr = or();
     if (match(EQUAL)) {
       Token equals = previous();
       Expr value = assignment();
@@ -33,6 +33,24 @@ class Parser {
         return new Expr.Assign(name, value);
       }
       error(equals, "Invalid assignment target.");
+    }
+    return expr;
+  }
+  private Expr or () {
+    Expr expr = and();
+    while (match(OR)) {
+      Token operator = previous();
+      Expr right = and();
+      expr = new Expr.Logical(expr, operator, right);
+    }
+    return expr;
+  }
+  private Expr and () {
+    Expr expr = equality();
+    while (match(AND)) {
+      Token operator = previous();
+      Expr right = equality();
+      expr = new Expr.Logical(expr, operator, right);
     }
     return expr;
   }
@@ -46,8 +64,20 @@ class Parser {
     }
   }
   private Stmt statment() {
+    if (match(IF)) return ifStatement();
     if (match(PRINT)) return printStatement();
     return expressionStatement();
+  }
+  private Stmt ifStatement () {
+    consume(LEFT_PAREN, "Expect '(' after 'if'.");
+    Expr condition = expression();
+    consume(RIGHT_PAREN, "Expect ')' after if condition.");
+    Stmt thenBranch = statement();
+    Stmt elseBranch = null;
+    if (match(ELSE)) {
+      elseBranch = statement();
+    }
+    return new Stmt.If(condition, thenBranch, elseBranch);
   }
   private Stmt printStatement () {
     Expr value = expression();
