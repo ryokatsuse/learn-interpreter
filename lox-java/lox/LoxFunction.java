d0f0cd6c42d81b37a4e6c28c50f15e7ffd7085f0
ryokatsu <ryokatsu0719@gmail.com>
2024-01-02 00:27:23 +0900

    fix:クロージャを考慮して親を特定できるように

--- lox-java/lox/LoxFunction.java
+++ lox-java/lox/LoxFunction.java
@@ -3,8 +3,10 @@ import java.util.List;
 
 class LoxFunction implements LoxCallable {
   private final Stmt.Function declaration;
+  private final Environment closure;
   LoxFunction(Stmt.Function declaration) {
     this.declaration = declaration;
+    this.closure = null;
   }
   @Override
   public String toString() {
@@ -16,7 +18,7 @@ class LoxFunction implements LoxCallable {
   }
   @Override
   public Object call(Interpreter interpreter, List<Object> arguments) {
-    Enviroment environment = new Enviroment(interpreter.globals);
+    Environment environment = new Environment(closure);
     for (int i = 0; i < declaration.params.size(); i++) {
       environment.define(declaration.params.get(i).lexeme, arguments.get(i));
     }
